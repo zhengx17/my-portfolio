000a2404db3f42ad216e2e593cef9cf0fb01dc57
zhengx17 <zhengxing@pku.edu.cn>
2020-07-13 01:23:20 -0400

    Add blog to main page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['my favorite, ping-pong', 'play badminton sometimes', 'recently learned to play go'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
